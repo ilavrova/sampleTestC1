cb6cc240091c3065941989cb6505e5f5e55115e2
ilavrova <irina.lav78@gmail.com>
2016-01-14 01:02:14 +0200

    Added tests

--- src/main/java/Calculate.java
+++ src/main/java/Calculate.java
@@ -11,6 +11,24 @@ public class Calculate {
     public int add(int a, int b) {
         return a + b;
     }
+    public double add(double a, double b) { return a + b; }
+
+
+    public int sub (int a, int b) {
+        return a - b;
+    }
+
+
+    public int mult (int a, int b) {
+        return a * b;
+    }
+
+    public int div (int a, int b)  {return a / b; }
+
+    public double Sqrt(double d) { return Math.sqrt(d);}
+
+    public int sqr (int x)  {return x*x; }
+
 
     //TODO
     //add methods for multiply, division, subtraction, square root, x^2
--- src/test/java/AddTwoValuesTest.java
+++ src/test/java/AddTwoValuesTest.java
@@ -1,3 +1,6 @@
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -19,13 +22,60 @@ public class AddTwoValuesTest {
     }
 
     @Test
-    public void testAddTwoPositiveValues() {
+    public void testdivTwoPositiveValues() {
         // Class for testing
         Calculate calc = new Calculate();
 
         // Method to check
-        assertEquals("10 + 5 must be 15", 15, calc.add(10, 5));
+        assertEquals("25 / 5 must be 5", 5, calc.div (25, 5));
     }
+
+    @Test
+    public void testAddTwoNegativValues() {
+        // Class for testing
+        Calculate calc = new Calculate();
+
+        // Method to check
+        assertEquals("-10 + -5 must be -15", -15, calc.add(-10, -5));
+    }
+
+    @Test
+    public void testAddNegativandPositiveValues() {
+        // Class for testing
+        Calculate calc = new Calculate();
+
+        // Method to check
+        assertEquals("-10 + 5 must be -5", -5, calc.add(-10, 5));
+    }
+
+    @Test
+    public void testsubPositiveValues() {
+        // Class for testing
+        Calculate calc = new Calculate();
+
+        // Method to check
+        assertEquals("20 - 5 must be 15", 15, calc.sub(20, 5));
+    }
+
+    @Test
+    public void testmultPositiveValues() {
+        // Class for testing
+        Calculate calc = new Calculate();
+
+        // Method to check
+        assertEquals("5 * 5 must be 25", 25, calc.mult(5, 5));
+    }
+
+    @Test
+    public void testdivPositiveValues() {
+        // Class for testing
+        Calculate calc = new Calculate();
+
+        // Method to check
+        assertEquals("25 / 5 must be 5", 5, calc.div(25, 5));
+    }
+
+
 
     @Test
     public void testAddTwoNegativeValues() {
@@ -37,6 +87,25 @@ public class AddTwoValuesTest {
 
     }
 
+    @Test
+    public void testSqrtValues() {
+        // Class for testing
+        Calculate calc = new Calculate();
+
+        // Method to check
+        Assert.assertEquals("Sqrt from 25 must be 5", 5, calc.Sqrt(25), 0) ;
+
+    }
+
+    @Test
+    public void testSqrValues() {
+        // Class for testing
+        Calculate calc = new Calculate();
+
+        // Method to check
+        assertEquals("Sqr 6 must be 36", 36, calc.sqr(6));
+
+    }
     //TODO
     //add new 5 tests for add() method
 
